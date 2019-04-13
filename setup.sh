53ca5e35f611debc8c3907469245c43c1bf13cde
kei-cs <keisuke.cs@gmail.com>
2019-04-14 06:56:21 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,35 +3,57 @@
 # Install homebrew and homebrew cask
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-
+brew doctor
 # set homebrew cask installing directroy
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
 # install git
 brew install git
 git --version
-
 # install vim
 brew install vim --with-override-system-vi
-
 # install fish
-# install iterm2
+brew install fish
+curl -L https://get.oh-my.fish | fish
 # install peco
+brew install peco
 # install tmux
-# install shutto
+brew install tmux
 # install golang
+brew install go
+export GOPATH=${HOME}/Dev/go
+export PATH=$GOPATH/bin:$PATH
 # install python3
-# install ruby
+brew install python
 # install rbenv
-# install bundler
+brew install rbenv
+# install ruby-build
+brew install ruby-build
+# install readline
+brew install readline
 # install SpaceVim
-# install atom
+curl -sLf https://spacevim.org/install.sh | bash
 # install docker
+brew install docker
+brew cask install docker
 # install k8s
-# install virtualbox
-# install sequelpro
-# install gore
+brew install kubernetes-cli
 # install httpie
-# install venv
+brew install httpie
 # install tig
+brew install tig
 # install ag
+brew install ag
+# install google chrome
+brew cask install google-chrome
+# install iterm2
+brew cask install iterm2
+# install alfred
+brew cask install alfred
+# install sequel-pro
+brew cask install sequel-pro
+# install slack
+brew cask install slack
+# install virtualbox
+brew cask install virtualbox
+# install atom
+brew cask install atom
